def0c43bf1990a25583dea4fdf3fc3395cb67a17
makingthingsforfriends <yuwei.s@hotmail.co.uk>
2022-03-14 15:27:19 +1300

    Completed header component

--- yuwei-website/src/components/header/Header.jsx
+++ yuwei-website/src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/profilepic.jpeg'
+import ME from '../../assets/profilepic.png'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
